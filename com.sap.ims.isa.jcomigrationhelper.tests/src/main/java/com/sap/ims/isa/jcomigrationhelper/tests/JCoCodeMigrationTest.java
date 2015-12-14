656216e5807c9b68389cd51be70219d5194484e4
Iwan Zarembo <iwan.zarembo@sap.com>
2015-12-14 08:10:50 +0100

    created a new test structure to support multiple tests

--- com.sap.ims.isa.jcomigrationhelper.tests/src/main/java/com/sap/ims/isa/jcomigrationhelper/tests/JCoCodeMigrationTest.java
+++ com.sap.ims.isa.jcomigrationhelper.tests/src/main/java/com/sap/ims/isa/jcomigrationhelper/tests/JCoCodeMigrationTest.java
@@ -2,35 +2,17 @@ package com.sap.ims.isa.jcomigrationhelper.tests;
 
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Hashtable;
 
-import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IProjectDescription;
-import org.eclipse.core.resources.IWorkspace;
-import org.eclipse.core.resources.IWorkspaceRunnable;
-import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.IPath;
-import org.eclipse.core.runtime.Path;
-import org.eclipse.jdt.core.IClasspathEntry;
 import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.IPackageFragment;
-import org.eclipse.jdt.core.IPackageFragmentRoot;
-import org.eclipse.jdt.core.JavaCore;
-import org.eclipse.jdt.core.formatter.DefaultCodeFormatterConstants;
-import org.eclipse.jdt.internal.compiler.impl.CompilerOptions;
-import org.eclipse.jdt.internal.core.JavaProject;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import com.sap.ims.isa.jcomigrationhelper.imports.ImportOrganizerTask;
 
-public class JCoCodeMigrationTest {
+public class JCoCodeMigrationTest extends TestsSuiteParent {
 
-
-    protected IPackageFragmentRoot srcFolder;
-    java.nio.file.Path             resourcesPath = Paths.get("src", "main", "resources");
 
     @Test
     public void jcoCodeMigrationForCompilationUnit() throws Exception {
@@ -47,7 +29,7 @@ public class JCoCodeMigrationTest {
 
         ImportOrganizerTask task = new ImportOrganizerTask(null);
         task.setMonitor(new DummyProgressMonitor());
-        task.processCompulationUnit(cu);
+        task.processCompilationUnit(cu);
 
         java.nio.file.Path resultSourceFilePath = this.resourcesPath.resolve("OldJCoAllInOne.java.resource.result");
         String migResult = new String(Files.readAllBytes(resultSourceFilePath), StandardCharsets.UTF_8);
@@ -58,49 +40,7 @@ public class JCoCodeMigrationTest {
 
     @Before
     public void setUp() throws Exception {
-        IWorkspace workspace = ResourcesPlugin.getWorkspace();
-
-        final IProject project = workspace.getRoot().getProject("migrationTestProject");
-        IWorkspaceRunnable create = monitor -> {
-            project.create(null);
-            project.open(null);
-
-            IProjectDescription description = project.getDescription();
-            description.setNatureIds(new String[] {JavaCore.NATURE_ID});
-            project.setDescription(description, null);
-
-            IPath projectPath = project.getFullPath();
-
-            // create source folder
-            IPath sourcePath = new Path("src");
-            project.getFolder(sourcePath).create(true, true, null);
-
-            // create project output folder
-            IPath outputPath = new Path("bin");
-            project.getFolder(outputPath).create(true, true, null);
-
-            // add the source folder
-            IClasspathEntry sourceEntry = JavaCore.newSourceEntry(projectPath.append(sourcePath), null,
-                    projectPath.append(outputPath));
-
-            // make sure the settings are always the same for the tests
-            @SuppressWarnings("unchecked")
-            Hashtable<String, String> options = JavaCore.getOptions();
-            options.put(DefaultCodeFormatterConstants.FORMATTER_TAB_CHAR, JavaCore.SPACE);
-            options.put(DefaultCodeFormatterConstants.FORMATTER_TAB_SIZE, "4");
-            JavaCore.setOptions(options);
-
-            JavaProject javaProject = (JavaProject) JavaCore.create(project);
-            javaProject.writeFileEntries(new IClasspathEntry[] { sourceEntry }, projectPath.append(outputPath));
-            options = new Hashtable<>();
-            options.put(CompilerOptions.OPTION_Compliance, CompilerOptions.VERSION_1_8);
-            options.put(CompilerOptions.OPTION_Source, CompilerOptions.VERSION_1_8);
-            options.put(CompilerOptions.OPTION_TargetPlatform, CompilerOptions.VERSION_1_8);
-            javaProject.setOptions(options);
-
-            this.srcFolder = javaProject.getPackageFragmentRoot(sourcePath);
-        };
-        workspace.run(create, null);
+        this.createProject("migrate_imports_project");
     }
 
 }
---
+++ com.sap.ims.isa.jcomigrationhelper.tests/src/main/java/com/sap/ims/isa/jcomigrationhelper/tests/TestsSuiteParent.java
@@ -0,0 +1,85 @@
+package com.sap.ims.isa.jcomigrationhelper.tests;
+
+import java.nio.file.Paths;
+import java.util.Hashtable;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectDescription;
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.IWorkspaceRunnable;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.jdt.core.IClasspathEntry;
+import org.eclipse.jdt.core.IPackageFragmentRoot;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.formatter.DefaultCodeFormatterConstants;
+import org.eclipse.jdt.internal.compiler.impl.CompilerOptions;
+import org.eclipse.jdt.internal.core.JavaProject;
+
+public abstract class TestsSuiteParent {
+
+    /**
+     * The source folder of the project which has been created.
+     */
+    protected IPackageFragmentRoot srcFolder;
+    protected java.nio.file.Path   resourcesPath = Paths.get("src", "main", "resources");
+
+    public TestsSuiteParent() {
+    }
+
+    /**
+     * Creates a project with the specified name in the workspace which has a source (src) and an output (bin) folder.
+     * The source folder will be put into the variable {@link #srcFolder}.
+     *
+     * @param projectName
+     *            The project name to create.
+     * @throws Exception
+     *             In case of any error.
+     */
+    protected void createProject(String projectName) throws Exception {
+        IWorkspace workspace = ResourcesPlugin.getWorkspace();
+
+        final IProject project = workspace.getRoot().getProject(projectName);
+        IWorkspaceRunnable create = monitor -> {
+            project.create(null);
+            project.open(null);
+
+            IProjectDescription description = project.getDescription();
+            description.setNatureIds(new String[] { JavaCore.NATURE_ID });
+            project.setDescription(description, null);
+
+            IPath projectPath = project.getFullPath();
+
+            // create source folder
+            IPath sourcePath = new Path("src");
+            project.getFolder(sourcePath).create(true, true, null);
+
+            // create project output folder
+            IPath outputPath = new Path("bin");
+            project.getFolder(outputPath).create(true, true, null);
+
+            // add the source folder
+            IClasspathEntry sourceEntry = JavaCore.newSourceEntry(projectPath.append(sourcePath), null,
+                    projectPath.append(outputPath));
+
+            // make sure the settings are always the same for the tests
+            @SuppressWarnings("unchecked")
+            Hashtable<String, String> options = JavaCore.getOptions();
+            options.put(DefaultCodeFormatterConstants.FORMATTER_TAB_CHAR, JavaCore.SPACE);
+            options.put(DefaultCodeFormatterConstants.FORMATTER_TAB_SIZE, "4");
+            JavaCore.setOptions(options);
+
+            JavaProject javaProject = (JavaProject) JavaCore.create(project);
+            javaProject.writeFileEntries(new IClasspathEntry[] { sourceEntry }, projectPath.append(outputPath));
+            options = new Hashtable<>();
+            options.put(CompilerOptions.OPTION_Compliance, CompilerOptions.VERSION_1_8);
+            options.put(CompilerOptions.OPTION_Source, CompilerOptions.VERSION_1_8);
+            options.put(CompilerOptions.OPTION_TargetPlatform, CompilerOptions.VERSION_1_8);
+            javaProject.setOptions(options);
+
+            this.srcFolder = javaProject.getPackageFragmentRoot(sourcePath);
+        };
+        workspace.run(create, null);
+    }
+}
